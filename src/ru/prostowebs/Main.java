71ed948c58cdce4d09a440cabab002135f41d9e4
Ivan <vpela2016@mail.ru>
2020-12-23 12:50:35 +0300

    added inital structure
    wrote some code)

--- src/ru/prostowebs/Main.java
+++ src/ru/prostowebs/Main.java
@@ -1,8 +1,31 @@
 package ru.prostowebs;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Ipelcher\'s Minecraft ModPack System!");
+
+        System.out.print("Please enter full path to ModPack directory: ");
+        File mpfdir1 = new File(scanner.nextLine());
+        if (!mpfdir1.exists()) {
+            mpfdir1.mkdir();
+        }
+        System.out.println("Options: ");
+        System.out.println("1 - Create ModPack");
+        System.out.println("2 - Choose ModPack");
+        System.out.println("");
+        System.out.print("Choose what you want to do: ");
+        int option = scanner.nextInt();
+        if (option == 1) {
+            //code1
+        } else if (option == 2) {
+            //code2
+        } else {
+            System.out.println("ERROR: This option isn't exists!");
+        }
     }
 }
---
+++ src/ru/prostowebs/cmp.java
@@ -0,0 +1,16 @@
+package ru.prostowebs;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class cmp {
+    public static void createModPack(File mpfdir1) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Create new ModPack: ");
+        System.out.println("MP Name: ");
+        String mpname = scanner.nextLine();
+        System.out.println("MP Version");
+        String mpver = scanner.nextLine();
+        Sys
+    }
+}
